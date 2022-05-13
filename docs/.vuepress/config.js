0384bfe92813c76edf778a04234831cbdeb81778
Lee Li Wei <leeliwei930@gmail.com>
2022-05-13 22:52:16 +0800

    fix links not show up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,28 @@
+const {
+    activeHeaderLinksPlugin,
+} = require("@vuepress/plugin-active-header-links");
+const { defaultTheme } = require("vuepress");
+
 module.exports = {
-	title: "ITS30605",
-	description: "ITS30605 Web Programming Practical Labs",
+    title: "ITS30605",
+    description: "ITS30605 Web Programming Practical Labs",
 
-	themeConfig: {
-		editLinks: false,
-		lastUpdated: true,
-		// logo: "https://vuejs.org/images/logo.png",
-		sidebarDepth: 4,
-		navbar: [
-			{ text: "WP", link: "/" },
-			{ text: "OOP", link: "https://deezombiedude612.github.io/oop-labs" },
-		],
-	},
-
-	plugins: ["@vuepress/plugin-active-header-links@next"],
+    theme: defaultTheme({
+        editLinks: false,
+        lastUpdated: true,
+        // logo: "https://vuejs.org/images/logo.png",
+        sidebarDepth: 4,
+        navbar: [
+            { text: "WP", link: "/" },
+            {
+                text: "OOP",
+                link: "https://deezombiedude612.github.io/oop-labs",
+            },
+        ],
+    }),
+    plugins: [
+        activeHeaderLinksPlugin({
+            // options
+        }),
+    ],
 };
